5f01e322fa1c12e2d46dc9ca8397f084f3d8fb3f
raghu k <raghuk@eharmony.com>
2018-05-10 19:45:33 -0700

    init

---
+++ app/src/main/java/weather/co/MainActivity.java
@@ -0,0 +1,17 @@
+package weather.co;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import timber.log.Timber;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Timber.e("Start Action");
+    }
+}
---
+++ app/src/main/java/weather/co/WeatherApp.java
@@ -0,0 +1,16 @@
+package weather.co;
+
+import android.app.Application;
+
+import timber.log.Timber;
+import timber.log.Timber.DebugTree;
+
+public class WeatherApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Timber.plant(new DebugTree());
+    }
+}
